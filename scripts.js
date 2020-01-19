59510719f9b89954b6be32e3ff4928521080ba65
Luci Peterson <lucijpeterson@gmail.com>
2020-01-18 18:32:08 -0800

    Added modal displaying employee details
    Added window that displays employee details when you click on a card.
    
    The window closes when you click the 'X' button.
    
    Changed number of query results from 10 to 12.

--- scripts.js
+++ scripts.js
@@ -1,7 +1,8 @@
 //employee-directory/scripts.js
-const apiURL = "https://randomuser.me/api/?results=10";
+const apiURL = "https://randomuser.me/api/?results=12";
 const request = new XMLHttpRequest();
 const gallery = document.getElementById('gallery');
+const app = document.body;
 
 //REQUEST DATA FROM API
 request.open('GET', apiURL, true);
@@ -32,6 +33,40 @@ request.onload = function () {
         card.appendChild(cardInfo);
         cardInfo.appendChild(h1);
         cardInfo.appendChild(p);
+        card.addEventListener('click', function(event) {
+            //CREATE MODAL ELEMENTS
+            const modalContainer = document.createElement('div');
+            const modal = document.createElement('div');
+            const closeBtn = document.createElement('button');
+            const modalInfo = document.createElement('div');
+            const modalImg = document.createElement('img');
+            const h3 = document.createElement('h3');
+            const p = document.createElement('p');
+            //SET MODAL ELEMENT ATTRIBUTES
+            modalContainer.setAttribute('class','modal-container');
+            modal.setAttribute('class','modal');
+            closeBtn.innerHTML = '<button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>';
+            closeBtn.addEventListener('click', function(event){modalContainer.style.display = 'none'})
+            modalInfo.setAttribute('class','modal-info-container');
+            modalImg.innerHTML = `<img class="modal-img" src=${user.picture.medium} alt="profile picture">`;
+            h3.setAttribute('class','modal-name cap');
+            h3.textContent = user.name.first + ' ' + user.name.last;
+            p.setAttribute('class','modal-text');
+            p.textContent = 
+            `${user.email}
+            ${user.location.city}
+            ${user.phone}
+            ${user.location.street.number} ${user.location.street.name} ${user.location.city}, ${user.location.state}
+            Birthday: ${user.dob.date}`
+            //APPEND MODAL ELEMENTS TO PAGE
+            app.appendChild(modalContainer);
+            modalContainer.appendChild(modal);
+            modal.appendChild(closeBtn);
+            modal.appendChild(modalInfo);
+            modalInfo.appendChild(modalImg);
+            modalInfo.appendChild(h3);
+            modalInfo.appendChild(p);
+            })
         });
     } else { //IF THE REQUEST DOESN'T WORK, DISPLAY AN ERROR MESSAGE
     const errorMessage = document.createElement('marquee');
