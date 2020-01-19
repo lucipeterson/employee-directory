3cff4e64c606deb66f20adf444beb46e1316b79a
Luci Peterson <lucijpeterson@gmail.com>
2020-01-18 19:29:10 -0800

    Modal formatting adjustments
    Changed the formatting of the employee information displayed in the pop-up window by creating a new paragraph for each line and taking away the unnecessary characters at the end of the DOB.  Also added some missing punctuation.

--- scripts.js
+++ scripts.js
@@ -3,6 +3,10 @@ const apiURL = "https://randomuser.me/api/?results=12";
 const request = new XMLHttpRequest();
 const gallery = document.getElementById('gallery');
 const app = document.body;
+const searchForm = document.createElement('form');
+searchForm.innerHTML = '<form action="#" method="get"><input type="search" id="search-input" class="search-input" placeholder="Search..."><input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit"></form>';
+const searchContainer = document.querySelector('.search-container');
+searchContainer.appendChild(searchForm);
 
 //REQUEST DATA FROM API
 request.open('GET', apiURL, true);
@@ -16,7 +20,7 @@ request.onload = function () {
         card.setAttribute('class', 'card');
         //CREATE CONTAINER TO HOLD INFO INSIDE OF CARD
         const cardInfo = document.createElement('div');
-        cardInfo.setAttribute('class','card-info-container')
+        cardInfo.setAttribute('class','card-info-container');
         //GET PIC FROM API
         const profilePic = document.createElement('div');
         profilePic.innerHTML = `<img class="card-img" src=${user.picture.thumbnail} alt="profile picture"></img>`;
@@ -41,7 +45,11 @@ request.onload = function () {
             const modalInfo = document.createElement('div');
             const modalImg = document.createElement('img');
             const h3 = document.createElement('h3');
-            const p = document.createElement('p');
+            const p2 = document.createElement('p');
+            const p3 = document.createElement('p');
+            const p4 = document.createElement('p');
+            const p5 = document.createElement('p');
+            const p6 = document.createElement('p');
             //SET MODAL ELEMENT ATTRIBUTES
             modalContainer.setAttribute('class','modal-container');
             modal.setAttribute('class','modal');
@@ -51,13 +59,16 @@ request.onload = function () {
             modalImg.innerHTML = `<img class="modal-img" src=${user.picture.medium} alt="profile picture">`;
             h3.setAttribute('class','modal-name cap');
             h3.textContent = user.name.first + ' ' + user.name.last;
-            p.setAttribute('class','modal-text');
-            p.textContent = 
-            `${user.email}
-            ${user.location.city}
-            ${user.phone}
-            ${user.location.street.number} ${user.location.street.name} ${user.location.city}, ${user.location.state}
-            Birthday: ${user.dob.date}`
+            p2.setAttribute('class','modal-text');
+            p2.textContent = `${user.email}`;
+            p3.setAttribute('class','modal-text cap');
+            p3.textContent = `${user.location.city}`;
+            p4.setAttribute('class','modal-text');
+            p4.textContent = `${user.cell}`
+            p5.setAttribute('class','modal-text');
+            p5.textContent = `${user.location.street.number} ${user.location.street.name} ${user.location.city}, ${user.location.state}`
+            p6.setAttribute('class','modal-text');
+            p6.textContent = `Birthday: ${user.dob.date.substr(0,9)}`
             //APPEND MODAL ELEMENTS TO PAGE
             app.appendChild(modalContainer);
             modalContainer.appendChild(modal);
@@ -65,7 +76,11 @@ request.onload = function () {
             modal.appendChild(modalInfo);
             modalInfo.appendChild(modalImg);
             modalInfo.appendChild(h3);
-            modalInfo.appendChild(p);
+            modalInfo.appendChild(p2);
+            modalInfo.appendChild(p3);
+            modalInfo.appendChild(p4);
+            modalInfo.appendChild(p5);
+            modalInfo.appendChild(p6);
             })
         });
     } else { //IF THE REQUEST DOESN'T WORK, DISPLAY AN ERROR MESSAGE
